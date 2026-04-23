38e7b74f57b9e59a89ad9f7aa6644f2deb5e7c9c
Andy Varshine <andy.varshine@gmail.com>
2026-04-23 01:54:29 +0000

    removed admin authorization for demo site

--- Pages/Admin/ElementStyleDesigner.cshtml.cs
+++ Pages/Admin/ElementStyleDesigner.cshtml.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
 
 namespace visualizer_demo.Pages.Admin
 {
-    [Authorize]
     public class ElementStyleDesignerModel : PageModel
     {
         public void OnGet()
         {
-            // Page load - authorization check already done via [Authorize] attribute
-            // All work is done on the client side with localStorage persistence
+            // Page load - all work is done on the client side with localStorage persistence
+            // Publicly accessible for anyone to design custom CSS classes
         }
     }
 }
